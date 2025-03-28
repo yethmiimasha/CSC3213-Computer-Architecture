ec8705e4beb509dece22407dc6bae5abab1ca056
yethmiimasha <139648583+yethmiimasha@users.noreply.github.com>
2025-03-28 01:47:42 +0000

    third asm file created

---
+++ Day 01/third.asm
@@ -0,0 +1,20 @@
+.model small
+.stack 100h
+
+.data
+msg db "Hello ",10,"Hi$"
+
+.code
+start:
+
+mov ax,@data
+mov ds,ax
+  
+mov dx, offset msg
+mov ah, 09h
+int 21h
+  
+mov ax,4c00h
+int 21h
+  
+end start
