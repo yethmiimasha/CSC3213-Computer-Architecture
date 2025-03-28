d5b270688d91389e68ca6a6bab7a73f1f2715aed
yethmiimasha <139648583+yethmiimasha@users.noreply.github.com>
2025-03-28 01:52:57 +0000

    second created

---
+++ Day 02/second.asm
@@ -0,0 +1,22 @@
+.model small
+.stack 100h
+
+.data
+m db 5
+n db 2
+.code
+start:
+mov ax,@data
+mov ds,ax
+
+mov dl,m
+sub dl,n
+add dl,48
+
+mov ah,02h
+int 21h
+
+mov ax,4c00h
+int 21h
+
+end start
